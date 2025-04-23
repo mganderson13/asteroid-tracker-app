e36abf49543eb088f3b67be056b92bdeef33b75f
Marianne Anderson <mganderson13@gmail.com>
2025-04-23 13:12:32 -0500

    adjusted item data access to account for arrays within data structure

--- app/components/AsteroidListCard.jsx
+++ app/components/AsteroidListCard.jsx
@@ -6,11 +6,11 @@ const AsteroidDisplay = ({item}) => (
     <Text>Name: {item.name}</Text>
     <Text>Diameter: {item.estimated_diameter.feet.estimated_diameter_min}-
     {item.estimated_diameter.feet.estimated_diameter_max} feet</Text>
-    <Text>Velocity: {item.close_approach_data.relative_velocity.miles_per_hour}mph</Text>
+    <Text>Velocity: {item.close_approach_data[0].relative_velocity.miles_per_hour}mph</Text>
     <Text>{item.is_potentially_hazardous_asteroid ? 
     "This asteroid is potentially hazardous!" 
     : "This asteroid is not hazardous"}</Text>
-    <Text>Miss distance: {item.close_approach_data.miss_distance.miles} miles</Text>
+    <Text>Miss distance: {item.close_approach_data[0].miss_distance.miles} miles</Text>
     </>
   )
 
